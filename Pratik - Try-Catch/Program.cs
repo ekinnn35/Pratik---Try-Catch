98dcbc5fafaae37d3364923d5851666c73c6eb74
ekinnn35 <ekinn.9193@gmail.com>
2025-02-06 13:44:14 +0300

    Add project files.

---
+++ Pratik - Try-Catch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Lütfen bir sayı giriniz: ");
+
+        try
+        {
+            int number = Convert.ToInt32(Console.ReadLine());  // Kullanıcıdan giriş al ve sayıya çevir
+            int square = number * number;  // Sayının karesini hesapla
+            Console.WriteLine($"Girilen sayının karesi: {square}");
+        }
+        catch (FormatException)  // Geçersiz giriş hatasını yakala
+        {
+            Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+        }
+    }
+}
